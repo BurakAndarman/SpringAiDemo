bb63ad7a41da117eadfa7d1df90d9dcd69fa74c6
BurakAndarman <brkandrmn@hotmail.com>
2024-08-02 00:10:16 +0300

    - Added daisyUI to frontend

---
+++ frontend/app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div data-theme="night">
+      Home
+      <button className="btn btn-primary">Hello There</button>
+    </div>
+  )
+}
+
+export default Home
